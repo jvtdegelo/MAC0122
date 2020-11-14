dc20ae57b96a78fb054907546cec237dce007297
Joao Victor Texeira Degelo <degelo@usp.br>
2020-11-14 13:46:21 -0300

    Adicionado o cabeçalho

--- EP4/ep4.c
+++ EP4/ep4.c
@@ -1,3 +1,36 @@
+/*    
+ \__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__
+
+  AO PREENCHER ESSE CABEÇALHO COM O MEU NOME E O MEU NÚMERO USP, 
+  DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESSE PROGRAMA. 
+  TODAS AS PARTES ORIGINAIS DESSE EXERCÍCIO-PROGRAMA (EP) FORAM 
+  DESENVOLVIDAS E IMPLEMENTADAS POR MIM SEGUINDO AS INSTRUÇÕES DESSE EP
+  E QUE PORTANTO NÃO CONSTITUEM PLÁGIO. DECLARO TAMBÉM QUE SOU RESPONSÁVEL
+  POR TODAS AS CÓPIAS DESSE PROGRAMA E QUE EU NÃO DISTRIBUI OU FACILITEI A
+  SUA DISTRIBUIÇÃO. ESTOU CIENTE QUE OS CASOS DE PLÁGIO SÃO PUNIDOS COM 
+  REPROVAÇÃO DIRETA NA DISCIPLINA.
+
+  Nome:Joao Victor Texeira Degelo   
+  NUSP:11803479
+
+  ep4.c
+
+  Referências: Com exceção das rotinas fornecidas no esqueleto e em sala
+  de aula, caso você tenha utilizado alguma referência, liste-as abaixo
+  para que o seu programa não seja considerada plágio.
+
+  - A biblioteca queue.h e item.h sao as bibliotecas que a professora 
+  desenvolveu durante a aula, e estao disponiveis em:
+  <https://www.ime.usp.br/~cris/aulas/20_2_122/programas/distancias/queue5/>,
+
+  - Foi anexado tambem um Makefile para compilar esse cliente, que necessita 
+  dos seguintes arquivos:
+  * ep4.c
+  * queue.c
+  * queue.h
+  * item.h 
+ \__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "item.h"
